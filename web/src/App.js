f4f5ad1ab41f23a17e6988d4bacb7eb8140b4951
Juan Vieira <jmvieirar@gmail.com>
2020-08-21 21:50:43 +1000

    basic react structure

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 // @ts-ignore
 import styles from './App.module.scss';
+import ParcelList from './components/ParcelList/ParcelList';
 
 function App() {
+  // properties
+
+  // methods
+
+  // render
   return (
     <div className={styles.app}>
       <header>
         <h1>Parcel Manager</h1>
       </header>
       <main>
-        <section></section>
+        <section>
+          <ParcelList parcels={[]} />
+        </section>
         <section></section>
       </main>
     </div>
--- web/src/components/ParcelList/ParcelList.js
+++ web/src/components/ParcelList/ParcelList.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// @ts-ignore
+import styles from './ParcelList.module.scss';
 
 const ParcelList = (props) => {
-  return <div></div>;
+  // properties
+  const { parcels = [] } = props;
+
+  // methods
+
+  // render
+  return (
+    <div className={styles.parcelList}>
+      <h1>Parcel List</h1>
+      // TODO: build list items
+    </div>
+  );
 };
 
-ParcelList.propTypes = {};
+ParcelList.propTypes = {
+  parcels: PropTypes.array.isRequired,
+};
 
 export default ParcelList;
