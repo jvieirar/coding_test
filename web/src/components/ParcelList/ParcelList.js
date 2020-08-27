fc8c8e1cfd6131ed3717346c9588e7c0b89a4f30
Juan <jmvieirar@gmail.com>
2020-08-27 22:02:53 +1000

    filters

--- web/src/components/ParcelList/ParcelList.js
+++ web/src/components/ParcelList/ParcelList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 // @ts-ignore
 import styles from './ParcelList.module.scss';
@@ -7,15 +7,44 @@ import ParcelItem from './ParcelItem';
 const ParcelList = (props) => {
   // properties
   const { parcels = [] } = props;
+  const [retailers, setRetailers] = useState([]);
+  const [filters, setFilters] = useState({ parcelId: '', retailerName: '', customerEmail: '' });
 
   // methods
+  const setFilterValue = (e) => {
+    e.persist();
+    setFilters((currentFilters) => ({ ...currentFilters, [e.target.name]: e.target.value }));
+  };
+
+  const applyFilters = (originalParcels) => {
+    return originalParcels.filter(
+      (parcel) =>
+        parcel.external_id.toUpperCase().includes(filters.parcelId.toUpperCase()) &&
+        parcel.retailer.toUpperCase().includes(filters.retailerName.toUpperCase()) &&
+        parcel.customer.toUpperCase().includes(filters.customerEmail.toUpperCase())
+    );
+  };
 
   // render
   return (
     <div className={styles.parcelList}>
       <h1>Parcel List</h1>
+      <div className={styles.filter}>
+        <label>
+          <span>Parcel id:</span>
+          <input type="text" name="parcelId" value={filters.parcelId} onChange={setFilterValue} />
+        </label>
+        <label>
+          <span>Retailer name:</span>
+          <input type="text" name="retailerName" value={filters.retailerName} onChange={setFilterValue} />
+        </label>
+        <label>
+          <span>Customer email:</span>
+          <input type="text" name="customerEmail" value={filters.customerEmail} onChange={setFilterValue} />
+        </label>
+      </div>
       <ul>
-        {parcels.map((parcel) => (
+        {applyFilters(parcels).map((parcel) => (
           <ParcelItem parcel={parcel} />
         ))}
       </ul>
